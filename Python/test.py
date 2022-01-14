2f70dd2faa61c53036645f905ee127bbb561e2e8
tylerleonard5 <tylerleonard@ufl.edu>
2022-01-13 21:53:58 -0500

    blackrock

---
+++ Python/test.py
@@ -0,0 +1,30 @@
+import sys
+from ast import literal_eval
+from itertools import product
+
+
+lines = sys.stdin.read().splitlines()
+
+if len(lines) != 3:
+    print("Invalid")
+
+else:
+    arr1 = literal_eval(lines[0])
+    arr2 = literal_eval(lines[1])
+    tup = literal_eval(lines[2])
+    isDigit = False
+    for i in arr1:
+        if (type(i) == int):
+            continue
+        elif (not i.isdigit()):
+            isDigit = True
+    for i in arr2:
+        if (type(i) == int):
+            continue
+        elif (not i.isdigit()):
+            isDigit = True
+            
+    if isDigit:
+        print('Invalid')
+    else:
+        print(list(product(arr1, arr2)))
